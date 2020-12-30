3ef98ad509dd88d9772e3c691b7d575273b7ec90
xxwangcw <593378212@qq.com>
2020-12-30 17:18:30 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyText.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import ohos.agp.components.Text;
+import ohos.app.Context;
+
+public class MyText extends Text {
+    public MyText(Context context) {
+        super(context);
+    }
+}
